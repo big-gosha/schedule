41e787016af463aba96f37927e46c60200fe1439
Big Gosha <patmandoge@gmail.com>
2025-03-03 07:42:57 -0300

    Update transformTool.js

--- js/transformTool.js
+++ js/transformTool.js
@@ -65,3 +65,28 @@ rotateBox.onmousedown = (e) => {
     document.addEventListener("mouseup", onMouseUp);
     e.stopPropagation();
 };
+
+////////////////////////////////////////////////////////
+// Turn white pixels into transparent
+//
+// thumbnail.canvas.ctx.drawImage(thumbnail.image, 
+//     0,
+//     0,
+//     512,
+//     512);
+//
+// let imageData = thumbnail.canvas.ctx.getImageData(0, 0, 512, 512);
+// let data = imageData.data;
+//
+// for (let i = 0; i < data.length; i += 4) {
+//     let threshold = 240;
+//     let firstDiff = data[i+0] - threshold;
+//     let secondDiff = data[i+1] - threshold;
+//     let thirdDiff = data[i+2] - threshold;
+//
+//     if (firstDiff >= 0 && secondDiff >= 0 && thirdDiff >= 0) {
+//         data[i + 3] = 0;
+//     }
+// }
+//
+// thumbnail.canvas.ctx.putImageData(imageData, 0, 0);
