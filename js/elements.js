f6ff65de805c5d9fc55da5737d29354c6df32e18
Big Gosha <patmandoge@gmail.com>
2025-03-11 02:55:33 -0300

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,12 +1,6 @@
 // Main canvas
 const root = document.body.children[0];
-// const editorCanvas = new Canvas('canvas-editor', screenWidth, screenHeight)
-// editorCanvas.element.width = screenWidth;
-// editorCanvas.element.height = screenHeight;
-
 const exportCanvas = new Canvas('canvas-export', bgWidth, bgHeight);
-// exportCanvas.element.width = 1920;
-// exportCanvas.element.height = 1080;
 
 // Images
 const bgImage = new Image();    bgImage.src = 'assets/BG.png';
@@ -15,14 +9,12 @@ const weekdayBgImage = new Image(); weekdayBgImage.src = 'assets/Box_online.png'
 // Export Button
 const exportButton = document.getElementById('export-button');
 exportButton.addEventListener("click", async function() {
-
-    artistName.drawAsCanvas(exportCanvas.ctx);
     week.forEach((day,i) => {
         day.setDrawAsCanvas(true)
     });
 
-    console.log('Drawing everything in the export canvas');
     drawCanvasGlobal();
+    artistName.drawAsCanvas(exportCanvas.ctx);
     await delay(200);
 
     let tempCanvas = document.createElement('canvas');
@@ -30,7 +22,6 @@ exportButton.addEventListener("click", async function() {
     tempCanvas.width = 1920; tempCanvas.height = 1080;
     ctx.drawImage(exportCanvas.element, 0, 0, 1920, 1080);
 
-    console.log('Drawing everything in the temp canvas');
     await delay(500);
 
     const link = document.createElement("a");
@@ -38,7 +29,6 @@ exportButton.addEventListener("click", async function() {
     link.href = tempCanvas.toDataURL("image/png");
     link.click();
 
-    console.log('Downloading canvas');
     await delay(300);
     drawCanvasGlobal();
 
@@ -46,12 +36,4 @@ exportButton.addEventListener("click", async function() {
     week.forEach((day,i) => {
         day.setDrawAsCanvas(false)
     });
-
-    console.log('End of button');
-    // link.href = exportCanvas.element.toDataURL("image/png");
-    // let scaleFactor = 2;
-    // exported.element.width = 1920 * scaleFactor;
-    // exported.element.height = 1080 * scaleFactor;
-    // exported.ctx.scale(scaleFactor, scaleFactor);
-    // exported.ctx.drawImage(editor.element, 0, 0, 200, 200);
 });
