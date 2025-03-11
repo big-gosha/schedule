2c5c4ee62073faf36babd9fca7e7a391f5fce727
Big Gosha <patmandoge@gmail.com>
2025-03-11 02:21:44 -0300

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -24,7 +24,7 @@ exportButton.addEventListener("click", async function() {
     let tempCanvas = document.createElement('canvas');
     let ctx = tempCanvas.getContext("2d");
     tempCanvas.width = 1920; tempCanvas.height = 1080;
-    tempCanvas.drawImage(exportCanvas.element, 0, 0, 1920, 1080);
+    ctx.drawImage(exportCanvas.element, 0, 0, 1920, 1080);
 
     await delay(500);
 
