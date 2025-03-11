7ebf3af5832615d38f8e3bb5b99f19c3384f8f84
Big Gosha <patmandoge@gmail.com>
2025-03-11 01:52:19 -0300

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,59 +1,41 @@
-// Main canvasas
+// Main canvas
 const root = document.body.children[0];
-const editorCanvas = new Canvas('canvas-editor', screenWidth, screenHeight)
+// const editorCanvas = new Canvas('canvas-editor', screenWidth, screenHeight)
+// editorCanvas.element.width = screenWidth;
+// editorCanvas.element.height = screenHeight;
+
 const exportCanvas = new Canvas('canvas-export', bgWidth, bgHeight);
-const backgroundCanvas = new Canvas('canvas-background', bgWidth, bgHeight);
-const foregroundCanvas = new Canvas('canvas-foreground', bgWidth, bgHeight);
+// exportCanvas.element.width = 1920;
+// exportCanvas.element.height = 1080;
 
 // Images
-const bgLeftImage = new Image();    bgLeftImage.src = 'assets/BG_Left.png';
-const bgRightImage = new Image();   bgRightImage.src = 'assets/BG_Right.png';
+const bgImage = new Image();    bgImage.src = 'assets/BG.png';
 const weekdayBgImage = new Image(); weekdayBgImage.src = 'assets/Box_online.png';
-
-// File Browser
-const fileBrowser = document.getElementById('file-browser');
-fileBrowser.addEventListener("change", function(event) {
-    let file = fileBrowser.files[0];
-    let reader = new FileReader();
-    if (!file) return;
-
-    reader.onload = function(e) {
-        thumbnail.image.onload = () => {
-            thumbnail.isLoaded = true;
-            thumbnail.reset(); 
-            drawCanvasGlobal();
-        }
-        thumbnail.image.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-});
 
 // Export Button
 const exportButton = document.getElementById('export-button');
-exportButton.addEventListener("click", function() {
-
+exportButton.addEventListener("click", async function() {
     // Say 'cheese' for the picture
     week.forEach((day,i) => {
-        day.switchHtml(exportCanvas.ctx)
+        day.setDrawAsCanvas(true)
     });
 
-    setTimeout(() => {
-        const link = document.createElement("a");
-        link.download = "canvas-image.png";
-        link.href = exportCanvas.element.toDataURL("image/jpeg", 0.5);
-        link.click();
-    }, 300);
+    await delay(700);
+
+    const link = document.createElement("a");
+    link.download = "canvas-image.png";
+    link.href = exportCanvas.element.toDataURL("image/png");
+    link.click();
+
+    await delay(300);
+
+    week.forEach((day,i) => {
+        day.setDrawAsCanvas(false)
+    });
 
     // let scaleFactor = 2;
     // exported.element.width = 1920 * scaleFactor;
     // exported.element.height = 1080 * scaleFactor;
     // exported.ctx.scale(scaleFactor, scaleFactor);
     // exported.ctx.drawImage(editor.element, 0, 0, 200, 200);
-});
-
-// Reset Button
-const resetButton = document.getElementById('reset-button');
-resetButton.addEventListener("click", function() {
-    thumbnail.reset();
-    drawCanvasGlobal();
 });
